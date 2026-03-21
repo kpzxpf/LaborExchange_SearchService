a95b047e589843276bda716249b846f32279919b
Nikita Volzhin <vlzh.nik@mail.ru>
2026-03-21 22:21:10 +0500

    fix test

--- src/test/java/com/volzhin/laborexchange_searchservice/LaborExchangeSearchServiceApplicationTests.java
+++ src/test/java/com/volzhin/laborexchange_searchservice/LaborExchangeSearchServiceApplicationTests.java
@@ -1,10 +1,19 @@
 package com.volzhin.laborexchange_searchservice;
 
+import com.volzhin.laborexchange_searchservice.repository.ResumeElasticRepository;
+import com.volzhin.laborexchange_searchservice.repository.VacancyElasticRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class LaborExchangeSearchServiceApplicationTests {
+
+    @MockBean
+    VacancyElasticRepository vacancyElasticRepository;
+
+    @MockBean
+    ResumeElasticRepository resumeElasticRepository;
 
     @Test
     void contextLoads() {
